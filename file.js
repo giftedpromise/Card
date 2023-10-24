3fe6785eaa402fe5a3b06425c5f3bf45316744db
Promise Nwankwo <giftedpromise1992@gmail.com>
2023-10-24 05:29:53 +0100

    worked on the profile section

---
+++ file.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+  const menu = document.querySelectorAll(".menu-links, menu-links-a");
+  const icon = document.querySelectorAll(".hambuger-icon");
+}
+menu.classList.toggle("open");
+icon.classList.toggle("open");
